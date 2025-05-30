b8398d9b19ecd21b26504751e843a0d3d159f2e2
jasminesamra <jasminekaursamra@gmail.com>
2025-05-30 13:43:34 -0700

    feat: add scrollable boxes

---
+++ src/lib/Scrolly.svelte
@@ -0,0 +1,22 @@
+<script>
+  // destructuring: take the element called "children" from props and assign it to the variable children
+  export const { children, articleTexts } = $props();
+  console.log(children);
+</script>
+
+<div>
+  <div>
+    {#each articleTexts as articleText}
+      <p class="article-text">{articleText}</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .article-text {
+    background-color: #f7f5eb;
+    padding: 50px;
+    margin-top: 50vh;
+    margin-bottom: 100vh;
+  }
+</style>
